11961566ec60bfecf88110f41652c9790c05bc61
YeshaDave <yeshadave9@gmail.com>
2019-10-06 09:46:28 -0700

    updated main.js

--- GrubHub/Frontend/src/components/Main.js
+++ GrubHub/Frontend/src/components/Main.js
@@ -4,8 +4,24 @@ import Navbar from './navbar/Navbar';
 import BuyerSignup from './signup/BuyerSignup';
 import BuyerLogin from './login/BuyerLogin';
 import OwnerLogin from './login/OwnerLogin';
-import OwnerSignup from './signup/OwnerSignup';
-import BuyerProfile from './profile/BuyerProfile'
+import OwnerProfile from './profile/OwnerProfile';
+import OwnerSignin from './signup/OwnerSignin';
+import BuyerProfile from './profile/BuyerProfile';
+import Editname from './profile/Editname';
+import EditPhone from './profile/EditPhone';
+import Phone from './profile/Phone';
+import MenuList from './owner/menuList';
+import UpdateItems from './owner/updateItems';
+import BuyerHome from './buyer/buyerHome';
+import Search from './buyer/search';
+import Details from './buyer/details'
+import UpcomingOrders from './buyer/upcomingOrders';
+import PastOrders from './buyer/pastOrders';
+import AddSections from './owner/addSection';
+import MenuItems from './owner/menuItems';
+import OPastOrders from './owner/OwnerPastOrders';
+import OUpcomingOrders from './owner/OwnerUpcomingOrders';
+
 
 class Main extends Component {
     render(){
@@ -18,6 +34,21 @@ class Main extends Component {
                 <Route path="/ologin" component={OwnerLogin}/>
                 <Route path="/osignup" component={OwnerSignup}/>
                 <Route path="/bprofile" component={BuyerProfile}/>
+                <Route path="/oprofile" component={OwnerProfile}/>
+                <Route path="/editName" component={Editname}/>
+                <Route path="/EditPhone" component={EditPhone}/>
+                <Route path="/MenuList" component={MenuList}/>
+                <Route path="/updateItems" component={UpdateItems}/>
+                <Route path="/BuyerHome" component={BuyerHome}/>
+                <Route path="/search" component={Search}/>
+                <Route path="/details" component={Details}/>
+                <Route path="/upcomingOrders" component={UpcomingOrders}/>
+                <Route path="/pastOrders" component={PastOrders}/>
+                <Route path="/addSection" component={AddSections}/>
+                <Route path="/menuItems" component={MenuItems}/>
+                <Route path="/phone" component={Phone}/>
+                <Route path="/oPastOrders" component={OPastOrders}/>
+                <Route path="/oUpcomingOrders" component={OUpcomingOrders}/>
             </div>
         )
     }
